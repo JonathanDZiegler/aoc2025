44341193aa8430e525ca0419effd0653e25e187a
Jonathan <jonathan@cradle.bio>
2025-12-31 15:57:32 +0100

    day 3 improvements

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -28,50 +28,60 @@ fn max_num(line: &str, n_digits: u8) -> Result<u64, Box<dyn std::error::Error>> 
             // assuming the current number is larger than the current position in digits
             if num >= digits[i] {
                 // we replace the current digit with the new one and hold the current digit in a buffer
-                let tmp = digits[i];
-                digits[i] = num;
-                num = tmp;
-            }
-            else{
-                break
+                (digits[i], num) = (num, digits[i]);
+            } else {
+                break;
             }
         }
     }
-    let mut result: u64 = 0;
-    // construct final number
-    for (exponent, digit) in digits.iter().rev().enumerate() {
-        result += *digit as u64 * 10u64.pow(exponent as u32);
-    }
+    // option 1: manual accumulation
+    // let mut result: u64 = 0;
+    // // construct final number
+    // for (exponent, digit) in digits.iter().rev().enumerate() {
+    //     result += *digit as u64 * 10u64.pow(exponent as u32);
+    // }
+
+    // option 2: using fold to accumulate the iterator. Cleaner intent and no mutable arguments
+    let result = digits
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0u64, |acc, (exp, &digit)| {
+            acc + digit as u64 * 10u64.pow(exp as u32)
+        });
+
     Ok(result)
 }
 
-fn run(input: &String, digits: u8)->Result<(), Box<dyn std::error::Error>> {
-    let mut max_nums = Vec::<u64>::new();
-    for line in input.lines() {
-        let curr_num = max_num(line, digits)?;
-        max_nums.push(curr_num as u64);
-    }
-    println!("Result: {}", max_nums.into_iter().sum::<u64>());
-    Ok(())
+fn run(input: &str, digits: u8) -> Result<u64, Box<dyn std::error::Error>> {
+    return Ok(input
+        .lines()
+        .map(|l| max_num(l, digits))
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .sum());
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let input = fs::read_to_string(path::Path::new("example_data.txt"))?;
     let input = get_input(Day(3), Year(2025))?;
-    println!("Part one:");
-    run(&input, 2)?;
-    println!("Part two:");
-    run(&input, 12)?;
+    println!("Part one: {}", run(&input, 2)?);
+
+    println!("Part two: {}", run(&input, 12)?);
     Ok(())
 }
 
-#[test]
-fn test_max_num() {
-    assert!(max_num("987654321111111", 2).unwrap() == 98);
-    assert!(max_num("811111111111119", 2).unwrap() == 89);
-    assert!(max_num("234234234234278", 2).unwrap() == 78);
-    assert!(max_num("818181911112111", 2).unwrap() == 92);
-    assert!(max_num("987654321111111", 12).unwrap() == 987654321111);
-    assert!(max_num("811111111111119", 12).unwrap() == 811111111119);
-    assert!(max_num("234234234234278", 12).unwrap() == 434234234278);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_max_num() {
+        assert_eq!(max_num("987654321111111", 2).unwrap(), 98);
+        assert_eq!(max_num("811111111111119", 2).unwrap(), 89);
+        assert_eq!(max_num("234234234234278", 2).unwrap(), 78);
+        assert_eq!(max_num("818181911112111", 2).unwrap(), 92);
+        assert_eq!(max_num("987654321111111", 12).unwrap(), 987654321111);
+        assert_eq!(max_num("811111111111119", 12).unwrap(), 811111111119);
+        assert_eq!(max_num("234234234234278", 12).unwrap(), 434234234278);
+    }
 }
